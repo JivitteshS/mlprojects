288afb443013e7bd2032431404a7f20cdda0fb4e
Jivittesh S <jivittesh@gmail.com>
2024-08-20 20:22:57 +0530

    modelhptuning

--- src/components/utils.py
+++ src/components/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 from sklearn.metrics import r2_score
 import pandas as pd
 from exception import CustomException
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -21,14 +22,20 @@ def save_object(file_path,obj):
 
 
 
-def evaluate_model(X_train,y_train,x_test,y_test,models):
+def evaluate_model(X_train,y_train,x_test,y_test,models,params):
     try:
         report={}
 
         for i in range(len(list(models))):
             model=list(models.values())[i]
+            param=params[list(models.keys())[i]]
 
-            model.fit(X_train,y_train).predict(X_train)
+            gs = GridSearchCV(model,param,cv=3)
+            gs.fit(X_train,y_train)
+            # model.fit(X_train,y_train).predict(X_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
 
             y_train_prod =model.predict(X_train)
 
